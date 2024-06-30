f49f068f7fe44cd41cc4c6a9b8513c0bba7639e6
Nikita Podvirnyi <krypt0nn@vk.com>
2024-06-30 05:46:28 +0200

    feat: implemented basic `BfsRecursionTraversal`

--- hyperborealib/src/server/traversal/bfs_recursion.rs
+++ hyperborealib/src/server/traversal/bfs_recursion.rs
@@ -1,5 +1,9 @@
+use std::collections::VecDeque;
+
 use crate::http::client::HttpClient;
 use crate::server::router::Router;
+
+use crate::rest_api::middleware::Client as ClientMiddleware;
 
 use super::{
     Traversal,
@@ -10,13 +14,19 @@ pub struct BfsRecursionTraversal;
 
 #[async_trait::async_trait]
 impl Traversal for BfsRecursionTraversal {
-    async fn traverse<T: HttpClient>(&self, server: &Server<T>) {
-        // let client = server.as_client();
+    async fn traverse<T: Router + Sync>(&self, http_client: impl HttpClient, server: &Server<T>) {
+        let client = ClientMiddleware::new(http_client, server.as_client());
 
-        // let mut servers = server.router().servers().await;
+        let mut remote_servers = VecDeque::from(server.router().servers().await);
 
-        // while let Some(server) = servers.pop() {
-        //     // client.
-        // }
+        while let Some(remote_server) = remote_servers.pop_front() {
+            if let Ok(mut response) = client.get_servers(&remote_server.address).await {
+                for remote_server in response.drain(..) {
+                    remote_servers.push_back(remote_server);
+                }
+            }
+
+            server.router().index_server(remote_server).await;
+        }
     }
 }
--- hyperborealib/src/server/traversal/mod.rs
+++ hyperborealib/src/server/traversal/mod.rs
@@ -1,13 +1,15 @@
-pub mod bfs_recursion;
-
 use crate::http::client::HttpClient;
 
+use super::router::Router;
 use super::Server;
+
+pub mod bfs_recursion;
 
 #[async_trait::async_trait]
 /// Traversal is a struct that implements network servers
 /// searching. It is called manually by the dev and intended
 /// to keep the updated state of the network servers.
 pub trait Traversal {
-    async fn traverse<T: HttpClient>(&self, server: &Server<T>);
+    /// Update network map using given server.
+    async fn traverse<T: Router + Sync>(&self, http_client: impl HttpClient, server: &Server<T>);
 }
