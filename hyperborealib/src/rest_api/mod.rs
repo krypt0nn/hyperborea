f50ab5974cc4c438848eb12a718b279a357a9690
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-03 20:52:29 +0200

    feat(hyperborealib): implement `AsJson` for more types

--- hyperborealib/src/rest_api/mod.rs
+++ hyperborealib/src/rest_api/mod.rs
@@ -123,7 +123,42 @@ pub trait AsJson {
     fn from_json(json: &Json) -> Result<Self, AsJsonError> where Self: Sized;
 }
 
+impl<T: AsJson> AsJson for Vec<T> {
+    fn to_json(&self) -> Result<Json, AsJsonError> {
+        let values = self.iter()
+            .map(T::to_json)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(serde_json::to_value(values)?)
+    }
+
+    fn from_json(json: &Json) -> Result<Self, AsJsonError> where Self: Sized {
+        let values = json.as_array()
+            .ok_or_else(|| AsJsonError::Other("array expected".into()))?
+            .iter()
+            .map(T::from_json)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(values)
+    }
+}
+
+impl<T: AsJson> AsJson for Box<T> {
+    #[inline]
+    fn to_json(&self) -> Result<Json, AsJsonError> {
+        self.as_ref().to_json()
+    }
+
+    #[inline]
+    fn from_json(json: &Json) -> Result<Self, AsJsonError> where Self: Sized {
+        Ok(Box::new(T::from_json(json)?))
+    }
+}
+
 #[macro_export]
+/// Implement `AsJson` to the types with
+/// implemented `serde::Serialize` and `serde::Deserialize`
+/// traits.
 macro_rules! impl_as_json {
     ($( $type:ty )*) => {
         $(
@@ -136,18 +171,8 @@ macro_rules! impl_as_json {
                     Ok(serde_json::from_value(json.clone())?)
                 }
             }
-
-            impl $crate::rest_api::AsJson for Vec<$type> {
-                fn to_json(&self) -> Result<serde_json::Value, $crate::rest_api::AsJsonError> {
-                    Ok(serde_json::to_value(())?)
-                }
-
-                fn from_json(json: &serde_json::Value) -> Result<Self, $crate::rest_api::AsJsonError> where Self: Sized {
-                    Ok(serde_json::from_value(json.clone())?)
-                }
-            }
         )*
-    };
+    }
 }
 
 impl_as_json!(
@@ -155,4 +180,5 @@ impl_as_json!(
     u8 u16 u32 u64 u128 usize
     i8 i16 i32 i64 i128 isize
     String
+    std::path::PathBuf
 );
