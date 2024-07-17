d23728f5059e52235d2eaafeb7fd6b5668009765
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-17 07:35:30 +0200

    feat(hyperborealib): added global `prelude` module

--- hyperborealib/src/lib.rs
+++ hyperborealib/src/lib.rs
@@ -4,6 +4,9 @@ pub mod http;
 pub mod drivers;
 pub mod rest_api;
 
+pub const STANDARD_VERSION: u64 = 1;
+pub const LIBRARY_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub mod exports {
     pub use k256;
     pub use rand_chacha;
@@ -19,5 +22,18 @@ pub mod exports {
     pub use tokio;
 }
 
-pub const STANDARD_VERSION: u64 = 1;
-pub const LIBRARY_VERSION: &str = env!("CARGO_PKG_VERSION");
+pub mod prelude {
+    pub use super::{
+        STANDARD_VERSION as HYPERBOREALIB_STANDARD,
+        LIBRARY_VERSION as HYPERBOREALIB_VERSION
+    };
+
+    pub use super::crypto::prelude::*;
+    pub use super::drivers::prelude::*;
+    pub use super::rest_api::prelude::*;
+
+    pub use super::time::*;
+    pub use super::http::*;
+
+    pub use super::impl_as_json;
+}
